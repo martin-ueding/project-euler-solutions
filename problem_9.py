089b0782dac2a53b12a75c93722418d300f50609
Martin Ueding <mu@martin-ueding.de>
2023-04-07 08:58:54 +0200

    feat(9): add solution in Python

--- problem_9.py
+++ problem_9.py
@@ -1,6 +1,14 @@
+def _solution_brute_force() -> int:
+    for a in range(1, 1000):
+        for b in range(1, 1000):
+            for c in range(1, 1000):
+                if a + b + c == 1000 and a**2 + b**2 == c**2:
+                    return a * b * c
+
+
 def solution_grid_search() -> int:
     for a in range(1, 1000):
-        for b in range(a, 1000 - a):
+        for b in range(a, 1000 - 2 * a):
             c = 1000 - a - b
             if a**2 + b**2 == c**2:
                 return a * b * c
