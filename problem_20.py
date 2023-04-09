2fba980637dbf12d5ab825325c66f64affb2a281
Martin Ueding <mu@martin-ueding.de>
2023-04-09 19:04:01 +0200

    Solve 31

--- problem_20.py
+++ problem_20.py
@@ -1,8 +1,20 @@
 import functools
 
 
-def solution() -> int:
+def solution_functional() -> int:
     return sum(map(int, str(functools.reduce(lambda a, b: a * b, range(1, 101)))))
+
+
+def solution_procedural() -> int:
+    factorial = 1
+    for factor in range(1, 101):
+        factorial *= factor
+
+    digit_sum = 0
+    for digit_char in str(factorial):
+        digit_sum += int(digit_char)
+
+    return digit_sum
 
 
 if __name__ == "__main__":
