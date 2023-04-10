a1a1cd0d2231beba9bb1edfbb14c988a332d6ed4
Martin Ueding <mu@martin-ueding.de>
2023-04-10 14:33:08 +0200

    Procedural one for 36

--- problem_36.py
+++ problem_36.py
@@ -12,6 +12,17 @@ def solution() -> int:
     return sum(filter(is_palindrome_base_2, filter(is_palindrome, range(1, 1_000_000))))
 
 
+def solution_procedural() -> int:
+    result = 0
+    for candidate in range(1, 1_000_000):
+        if not is_palindrome(candidate):
+            continue
+        if not is_palindrome_base_2(candidate):
+            continue
+        result += candidate
+    return result
+
+
 if __name__ == "__main__":
     import runner
 
