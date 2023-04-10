e9c0144c129255f4c80a5832707b1bb503413438
Martin Ueding <mu@martin-ueding.de>
2023-04-10 17:17:44 +0200

    Make 41 faster

--- problem_41.py
+++ problem_41.py
@@ -10,10 +10,25 @@ def is_pandigital(number: int) -> bool:
     return len(digits) == len(s)
 
 
-def solution() -> int:
+def _solution_sieve() -> int:
     return max(
         itertools.islice(filter(is_pandigital, reversed(prime_sieve(7_654_321))), 1)
     )
+
+
+def is_prime(number: int) -> bool:
+    for divisor in itertools.count(2):
+        if number % divisor == 0:
+            return False
+        if divisor * divisor > number:
+            return True
+
+
+def solution_permutations() -> int:
+    for digits in reversed(list(itertools.permutations("1234567"))):
+        number = int("".join(digits))
+        if is_prime(number):
+            return number
 
 
 if __name__ == "__main__":
