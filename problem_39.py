22b7da63121054c96e859611626273892a0ffdd7
Martin Ueding <mu@martin-ueding.de>
2023-04-10 16:02:45 +0200

    Optimize 39

--- problem_39.py
+++ problem_39.py
@@ -1,9 +1,11 @@
 def get_num_solutions(perimeter: int) -> int:
     num_solutions = 0
     for a in range(1, perimeter // 3):
-        for b in range(a, perimeter - 2 * a + 1):
-            c = perimeter - a - b
-            if a**2 + b**2 == c**2:
+        numerator = perimeter**2 - 2 * a * perimeter
+        denominator = 2 * (perimeter - a)
+        if numerator % denominator == 0:
+            b = numerator // denominator
+            if a <= b < 1000:
                 num_solutions += 1
     return num_solutions
 
