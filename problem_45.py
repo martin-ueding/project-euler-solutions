16e973002b3f7659e5765d05d036d168c432465c
Martin Ueding <mu@martin-ueding.de>
2023-04-10 19:32:57 +0200

    Solve 45

--- problem_45.py
+++ problem_45.py
@@ -1,12 +1,11 @@
 import itertools
-from problem_42 import is_triangle_number
 from problem_44 import is_pentagonal
 
 
 def solution() -> int:
     for n in itertools.count(144):
         hex_n = n * (2 * n - 1)
-        if is_pentagonal(hex_n) and is_triangle_number(hex_n):
+        if is_pentagonal(hex_n):
             return hex_n
 
 
