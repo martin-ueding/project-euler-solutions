00c8494a1cea68f3b73a549401e9dd177c480501
Martin Ueding <mu@martin-ueding.de>
2023-04-10 10:38:37 +0200

    Add brute force to 31

--- problem_31.py
+++ problem_31.py
@@ -1,4 +1,18 @@
+import itertools
+from tqdm import tqdm
+
 values = [200, 100, 50, 20, 10, 5, 2, 1]
+
+
+def _solution_brute_force() -> int:
+    num_partitions = 0
+    ranges = [range(200 // value + 1) for value in values]
+    print(ranges)
+    for counts in tqdm(itertools.product(*ranges)):
+        total_value = sum(value * count for value, count in zip(values, counts))
+        if total_value == 200:
+            num_partitions += 1
+    return num_partitions
 
 
 def count_partitions(used: tuple, remainder: int) -> int:
