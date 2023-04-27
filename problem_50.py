4a390c19d41d841597b35252636d098bfac763dc
Martin Ueding <mu@martin-ueding.de>
2023-04-27 20:59:58 +0200

    feat(50): finish solution in Python

--- problem_50.py
+++ problem_50.py
@@ -8,17 +8,17 @@ def solution() -> int:
     ceiling = 1_000_000
     summands = collections.defaultdict(lambda: 0)
     primes = prime_sieve(ceiling)
-    for begin in tqdm(range(len(primes))):
-        accumulator = 0
-        num_summands = 0
-        for prime in primes[begin:]:
-            accumulator += prime
-            if accumulator >= ceiling:
+    prime_set = set(primes)
+    max_prime = primes[-1]
+    for length in tqdm(range(len(primes) - 1, 0, -1)):
+        rolling_sum = sum(primes[:length])
+        for begin in range(len(primes) - length):
+            end = begin + length
+            rolling_sum += primes[end] - primes[begin]
+            if rolling_sum > max_prime:
                 break
-            num_summands += 1
-            if accumulator in primes:
-                summands[accumulator] = max(num_summands, summands[accumulator])
-    return max((num, prime) for prime, num in summands.items() if prime < ceiling)[1]
+            if rolling_sum in prime_set:
+                return rolling_sum
 
 
 if __name__ == "__main__":
