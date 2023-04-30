b85a930f6601b84e16306d80f1c72006387c5093
Martin Ueding <mu@martin-ueding.de>
2023-04-30 14:12:25 +0200

    Finish 57

--- problem_57.py
+++ problem_57.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Iterator
 
 from problem_33 import greatest_common_denominator
@@ -17,3 +18,19 @@ def square_root_series() -> Iterator[tuple[int, int]]:
         gcd = greatest_common_denominator(n, d)
         n //= gcd
         d //= gcd
+
+
+def solution() -> int:
+    terms_of_interest = list(
+        filter(
+            lambda term: len(str(term[0])) > len(str(term[1])),
+            itertools.islice(square_root_series(), 1000),
+        )
+    )
+    return len(terms_of_interest)
+
+
+if __name__ == "__main__":
+    import runner
+
+    runner.run(globals())
