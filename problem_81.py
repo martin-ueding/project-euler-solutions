93b307102c2731714b769509072163d2bd35e263
Martin Ueding <mu@martin-ueding.de>
2023-05-01 17:34:24 +0200

    feat(81): add solution in Python

---
+++ problem_81.py
@@ -0,0 +1,47 @@
+import json
+from typing import Iterator
+
+
+def read_file() -> list[list[int]]:
+    result = []
+    with open("data/p081_matrix.txt") as f:
+        for line in f:
+            result.append(json.loads(f"[{line}]"))
+    assert len(result) == 80
+    return result
+
+
+def iter_diagonally(size: int) -> Iterator[tuple[int, int]]:
+    row = size - 1
+    col = size - 1
+    while True:
+        yield row, col
+        col += 1
+        row -= 1
+        if row < 0:
+            row = col - 2
+            col = 0
+            if row < 0:
+                return
+        if col >= size:
+            col = row
+            row = size - 1
+
+
+def solution() -> None:
+    matrix = read_file()
+    size = 80
+    for row, col in iter_diagonally(size):
+        neighbors = []
+        if row < size - 1:
+            neighbors.append(matrix[row + 1][col])
+        if col < size - 1:
+            neighbors.append(matrix[row][col + 1])
+        matrix[row][col] += min(neighbors, default=0)
+    return matrix[0][0]
+
+
+if __name__ == "__main__":
+    import runner
+
+    runner.run(globals())
