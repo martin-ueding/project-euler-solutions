c59eb9be36b55a2735b14d4fa9d585fcb54898d4
Martin Ueding <mu@martin-ueding.de>
2023-05-15 10:46:24 +0200

    More 60

--- problem_60_test.py
+++ problem_60_test.py
@@ -1,10 +1,14 @@
-from problem_60 import find_tuple_of, has_property
+from problem_60 import find_tuple_of, has_property, ceiling
+from problem_7 import prime_sieve
 
 
 def test_has_property() -> None:
-    assert has_property(3, 7)
-    assert has_property(109, 3)
-    assert not has_property(3, 12)
+    primes = prime_sieve(ceiling)
+    prime_set = set(primes)
+    assert has_property(3, 7, prime_set)
+    assert has_property(109, 3, prime_set)
+    assert has_property(3, 673, prime_set)
+    assert not has_property(3, 12, prime_set)
 
 
 def test_solution() -> None:
