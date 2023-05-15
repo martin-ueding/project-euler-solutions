ff0c97c673e47709b0f0a8df921cb3b6de6cb731
Martin Ueding <mu@martin-ueding.de>
2023-05-15 10:46:24 +0200

    feat(60): continue solution in Python

--- problem_60.py
+++ problem_60.py
@@ -1,25 +1,28 @@
 import collections
-from problem_3 import prime_generator
-from problem_58 import is_prime_accelerated
+from problem_7 import prime_sieve
+
+ceiling = 10_000
 
 
-def has_property(first: int, second: int) -> bool:
-    return is_prime_accelerated(int(str(first) + str(second))) and is_prime_accelerated(
-        int(str(second) + str(first))
-    )
+def has_property(first: int, second: int, prime_set: set[int]) -> bool:
+    number_1 = int(str(first) + str(second))
+    number_2 = int(str(second) + str(first))
+    assert number_1 <= ceiling
+    assert number_2 <= ceiling
+    return number_1 in prime_set and number_2 in prime_set
 
 
 def find_tuple_of(size: int) -> int:
-    partners = collections.defaultdict(set)
-    for first_prime in prime_generator():
-        for second_prime in prime_generator():
-            if first_prime == second_prime:
+    partners = collections.defaultdict(list)
+    primes = prime_sieve(ceiling)
+    prime_set = set(primes)
+    for first_prime in primes:
+        for second_prime in primes:
+            if second_prime == first_prime:
                 break
-            if has_property(first_prime, second_prime):
-                partners[second_prime].add(first_prime)
-                if len(partners[second_prime]) == size - 1:
-                    print(second_prime, partners[second_prime])
-                    return sum(partners[second_prime])
+            if has_property(first_prime, second_prime, prime_set):
+                partners[second_prime].append(first_prime)
+    print(partners)
 
 
 def solution() -> None:
