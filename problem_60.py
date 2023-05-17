5d6ac6e4c1ce20148f3d584bebd6c7888faa5c9d
Martin Ueding <mu@martin-ueding.de>
2023-05-17 18:15:47 +0200

    feat(60): finish solution in Python

--- problem_60.py
+++ problem_60.py
@@ -2,17 +2,16 @@ import collections
 import itertools
 
 from tqdm import tqdm
+from problem_58 import is_prime_accelerated
 from problem_7 import prime_sieve
 
-ceiling = 1_000_000
+ceiling = 1000
 
 
-def has_property(first: int, second: int, prime_set: set[int]) -> bool:
+def has_property(first: int, second: int) -> bool:
     number_1 = int(str(first) + str(second))
     number_2 = int(str(second) + str(first))
-    assert number_1 <= ceiling
-    assert number_2 <= ceiling
-    return number_1 in prime_set and number_2 in prime_set
+    return is_prime_accelerated(number_1) and is_prime_accelerated(number_2)
 
 
 def _solution_full_grid() -> int:
@@ -22,20 +21,18 @@ def _solution_full_grid() -> int:
         for p2 in primes:
             if p2 == p1:
                 break
-            if has_property(p1, p2, prime_set):
+            if has_property(p1, p2):
                 for p3 in primes:
                     if p3 == p2:
                         break
-                    if has_property(p1, p3, prime_set) and has_property(
-                        p2, p3, prime_set
-                    ):
+                    if has_property(p1, p3) and has_property(p2, p3):
                         for p4 in primes:
                             if p4 == p3:
                                 break
                             if (
-                                has_property(p1, p4, prime_set)
-                                and has_property(p2, p4, prime_set)
-                                and has_property(p3, p4, prime_set)
+                                has_property(p1, p4)
+                                and has_property(p2, p4)
+                                and has_property(p3, p4)
                             ):
                                 return sum([p1, p2, p3, p4])
 
@@ -51,13 +48,12 @@ def create_partner_graph(all_partners: dict[int, list[int]]) -> None:
 
 def find_tuple_of(size: int) -> int:
     partners = collections.defaultdict(list)
-    primes = prime_sieve(ceiling)
-    prime_set = set(primes)
+    primes = prime_sieve(10000)
     for first_prime in tqdm(primes):
         for second_prime in primes:
             if second_prime == first_prime:
                 break
-            if has_property(first_prime, second_prime, prime_set):
+            if has_property(first_prime, second_prime):
                 partners[first_prime].append(second_prime)
         if len(partners[first_prime]) >= size - 1:
             for subset in itertools.combinations(partners[first_prime], size - 1):
@@ -73,7 +69,7 @@ def find_tuple_of(size: int) -> int:
 
 
 def solution() -> None:
-    return find_tuple_of(4)
+    return find_tuple_of(5)
 
 
 if __name__ == "__main__":
