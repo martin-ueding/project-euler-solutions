36d1fb65935befeba3ac5986c38013571df0327e
Martin Ueding <mu@martin-ueding.de>
2023-05-17 20:25:31 +0200

    Cleanup

--- problem_62.py
+++ problem_62.py
@@ -2,23 +2,10 @@ import collections
 import itertools
 from typing import Iterator
 
-from tqdm import tqdm
-
 
 def iter_cubes() -> Iterator[int]:
     for i in itertools.count(1):
         yield i**3
-
-
-class CubeSet:
-    def __init__(self) -> None:
-        self._cubes: list[int] = [0]
-        self._iterator = iter_cubes()
-
-    def is_cube(self, number: int) -> bool:
-        while self._cubes[-1] < number:
-            self._cubes.append(next(self._iterator))
-        return number in self._cubes
 
 
 def sort_digits(number: int) -> list[int]:
