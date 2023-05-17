b2d4ef9e0d736ab13108595de222339f076fe6fb
Martin Ueding <mu@martin-ueding.de>
2023-05-17 17:54:37 +0200

    refactor(60): improve Python solution

--- problem_60.py
+++ problem_60.py
@@ -1,7 +1,10 @@
 import collections
+import itertools
+
+from tqdm import tqdm
 from problem_7 import prime_sieve
 
-ceiling = 10_000
+ceiling = 1_000_000
 
 
 def has_property(first: int, second: int, prime_set: set[int]) -> bool:
@@ -12,21 +15,65 @@ def has_property(first: int, second: int, prime_set: set[int]) -> bool:
     return number_1 in prime_set and number_2 in prime_set
 
 
+def _solution_full_grid() -> int:
+    primes = prime_sieve(ceiling)
+    prime_set = set(primes)
+    for p1 in primes:
+        for p2 in primes:
+            if p2 == p1:
+                break
+            if has_property(p1, p2, prime_set):
+                for p3 in primes:
+                    if p3 == p2:
+                        break
+                    if has_property(p1, p3, prime_set) and has_property(
+                        p2, p3, prime_set
+                    ):
+                        for p4 in primes:
+                            if p4 == p3:
+                                break
+                            if (
+                                has_property(p1, p4, prime_set)
+                                and has_property(p2, p4, prime_set)
+                                and has_property(p3, p4, prime_set)
+                            ):
+                                return sum([p1, p2, p3, p4])
+
+
+def create_partner_graph(all_partners: dict[int, list[int]]) -> None:
+    with open("p060_graph.dot", "w") as f:
+        f.write("digraph {\noverlap=false\nsplines=true\n\n")
+        for number, partners in all_partners.items():
+            for partner in partners:
+                f.write(f"  {number} -> {partner}\n")
+        f.write("}\n")
+
+
 def find_tuple_of(size: int) -> int:
     partners = collections.defaultdict(list)
     primes = prime_sieve(ceiling)
     prime_set = set(primes)
-    for first_prime in primes:
+    for first_prime in tqdm(primes):
         for second_prime in primes:
             if second_prime == first_prime:
                 break
             if has_property(first_prime, second_prime, prime_set):
-                partners[second_prime].append(first_prime)
-    print(partners)
+                partners[first_prime].append(second_prime)
+        if len(partners[first_prime]) >= size - 1:
+            for subset in itertools.combinations(partners[first_prime], size - 1):
+                for i, number in enumerate(subset):
+                    for other_number in subset[:i]:
+                        if other_number not in partners[number]:
+                            break
+                    else:
+                        continue
+                    break
+                else:
+                    return first_prime + sum(subset)
 
 
 def solution() -> None:
-    return find_tuple_of(5)
+    return find_tuple_of(4)
 
 
 if __name__ == "__main__":
