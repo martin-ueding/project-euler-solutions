93388d3a12b4dfdedd2e2c8a9d622dffc10c3abb
Martin Ueding <mu@martin-ueding.de>
2023-05-18 19:39:29 +0200

    Solve 69

---
+++ problem_69.py
@@ -0,0 +1,16 @@
+from problem_3 import prime_generator
+
+
+def solution() -> int:
+    ceiling = 1000000
+    result = 1
+    for prime in prime_generator():
+        if result * prime > ceiling:
+            return result
+        result *= prime
+
+
+if __name__ == "__main__":
+    import runner
+
+    runner.run(globals())
