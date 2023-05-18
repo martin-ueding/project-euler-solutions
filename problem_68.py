705f49cd07925b9c665168d9d2e90d0291b89378
Martin Ueding <mu@martin-ueding.de>
2023-05-18 17:48:19 +0200

    Solve 68

--- problem_68.py
+++ problem_68.py
@@ -47,7 +47,61 @@ def triangle_solutions(coeffcients: list[int]) -> Iterator[list[int]]:
         coeffcients.pop()
 
 
+def pentagon_valid(numbers: list[int]) -> bool:
+    if len(numbers) > 9:
+        first = numbers[0] + numbers[1] + numbers[2]
+        second = numbers[3] + numbers[2] + numbers[4]
+        third = numbers[5] + numbers[4] + numbers[6]
+        fourth = numbers[7] + numbers[6] + numbers[8]
+        fifth = numbers[9] + numbers[8] + numbers[1]
+        return first == second == third == fourth == fifth
+    elif len(numbers) > 8:
+        first = numbers[0] + numbers[1] + numbers[2]
+        second = numbers[3] + numbers[2] + numbers[4]
+        third = numbers[5] + numbers[4] + numbers[6]
+        fourth = numbers[7] + numbers[6] + numbers[8]
+        return first == second == third == fourth
+    elif len(numbers) > 6:
+        first = numbers[0] + numbers[1] + numbers[2]
+        second = numbers[3] + numbers[2] + numbers[4]
+        third = numbers[5] + numbers[4] + numbers[6]
+        return first == second == third
+    elif len(numbers) > 4:
+        first = numbers[0] + numbers[1] + numbers[2]
+        second = numbers[3] + numbers[2] + numbers[4]
+        return first == second
+    else:
+        return True
 
+
+def pentagon_solution_string(coefficients: list[int]) -> int:
+    lines = [
+        (coefficients[0], coefficients[1], coefficients[2]),
+        (coefficients[3], coefficients[2], coefficients[4]),
+        (coefficients[5], coefficients[4], coefficients[6]),
+        (coefficients[7], coefficients[6], coefficients[8]),
+        (coefficients[9], coefficients[8], coefficients[1]),
+    ]
+    lines = shift_lines(lines)
+    return int("".join(map(str, (number for line in lines for number in line))))
+
+
+def pentagon_solutions(coeffcients: list[int]) -> Iterator[list[int]]:
+    if pentagon_valid(coeffcients):
+        if len(coeffcients) == 10:
+            yield pentagon_solution_string(coeffcients)
+    else:
+        return
+    for number in range(1, 10):
+        if number in coeffcients:
+            continue
+        coeffcients.append(number)
+        yield from pentagon_solutions(coeffcients)
+        coeffcients.pop()
+
+
+def solution() -> int:
+    return max(pentagon_solutions([10]))
 
 
 if __name__ == "__main__":
