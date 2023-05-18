775abbcf0409c81a8e611495590fffb63c4f069d
Martin Ueding <mu@martin-ueding.de>
2023-05-18 14:48:53 +0200

    Solve 64

--- problem_62_test.py
+++
@@ -1,8 +0,0 @@
-from problem_62 import CubeSet
-
-
-def test_cube_set() -> None:
-    cube_set = CubeSet()
-    assert cube_set.is_cube(41063625)
-    assert cube_set.is_cube(56623104)
-    assert cube_set.is_cube(66430125)
