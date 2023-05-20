94d163b74c165ffa4b8dcaa4e2f748aedea01a1b
Martin Ueding <mu@martin-ueding.de>
2023-05-20 09:38:16 +0200

    Improve 74

--- problem_74.py
+++ problem_74.py
@@ -1,7 +1,5 @@
 import functools
 
-from tqdm import tqdm
-
 
 @functools.cache
 def factorial(number: int) -> int:
@@ -11,22 +9,22 @@ def factorial(number: int) -> int:
     return result
 
 
-@functools.lru_cache(10_000_000)
+@functools.cache
 def factorial_digit_sum(number: int) -> int:
     return sum(factorial(int(digit)) for digit in str(number))
 
 
 def solution() -> int:
     result = 0
-    for number in tqdm(range(1_000_000)):
+    for number in range(1_000_000):
         steps = [number]
-        for iteration in range(65):
+        for iteration in range(60):
             new_number = factorial_digit_sum(steps[-1])
             if new_number in steps:
+                if iteration == 59:
+                    result += 1
                 break
             steps.append(new_number)
-        if len(steps) == 60:
-            result += 1
     return result
 
 
