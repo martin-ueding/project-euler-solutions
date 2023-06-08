dd95792f4f3bc32ebba48197abd796027b62c82f
Martin Ueding <mu@martin-ueding.de>
2023-06-08 20:28:00 +0200

    User simpler formula

--- solution_86.py
+++ solution_86.py
@@ -1,15 +1,9 @@
 import functools
 import itertools
-import math
 from typing import Optional
 
 from tqdm import tqdm
 
-
-def path_length(a: int, b: int, c: int) -> float:
-    return math.sqrt(((a * b) / (b + c)) ** 2 + b**2) + math.sqrt(
-        (a - (a * b) / (b + c)) ** 2 + c**2
-    )
 
 
 def integer_root(radicand: int) -> Optional[int]:
@@ -27,13 +21,9 @@ def integer_root(radicand: int) -> Optional[int]:
             result = new
 
 
-def shortest_path_is_integer(a, b, c) -> bool:
-    return shortest_path_cache(a, b + c)
-
-
 @functools.cache
-def shortest_path_cache(a, bc) -> bool:
-    return integer_root(a**2 + bc**2) is not None
+def shortest_path_is_integer(a, b_plus_c) -> bool:
+    return integer_root(a**2 + b_plus_c**2) is not None
 
 
 def solution() -> int:
@@ -41,13 +31,13 @@ def solution() -> int:
     result = 0
     with tqdm(total=ceiling) as pbar:
         for a in itertools.count(1):
-            for b in range(1, a + 1):
-                for c in range(1, b + 1):
-                    if shortest_path_is_integer(a, b, c):
-                        result += 1
-                        pbar.update(1)
-                        if result > ceiling:
-                            return a
+            for b_plus_c in range(2, 2*a + 1):
+                if shortest_path_is_integer(a, b_plus_c):
+                    num_b_c = b_plus_c // 2
+                    result += num_b_c
+                    pbar.update(num_b_c)
+                    if result > ceiling:
+                        return a
 
 
 if __name__ == "__main__":
