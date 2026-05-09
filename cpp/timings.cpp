9221db72baff8cb8d66ec91103c58610830f5fd1
Martin Ueding <mu@martin-ueding.de>
2026-05-09 08:54:54 +0200

    feat(C++): start with C++ version

---
+++ cpp/timings.cpp
@@ -0,0 +1,22 @@
+#include "timings.hpp"
+
+#include <iostream>
+
+void report_duration(
+    std::chrono::time_point<std::chrono::high_resolution_clock> const &start) {
+  auto const end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::milli> elapsed_ms = end - start;
+  std::chrono::duration<double, std::micro> elapsed_us = end - start;
+  std::chrono::duration<double, std::nano> elapsed_ns = end - start;
+  if (elapsed_ms.count() > 1) {
+    std::cout << "Duration: " << elapsed_ms.count() << " ms" << std::endl;
+  } else if (elapsed_us.count() > 1) {
+    std::cout << "Duration: " << elapsed_us.count() << " µs" << std::endl;
+  } else if (elapsed_ns.count() > 1) {
+    std::cout << "Duration: " << elapsed_ns.count() << " ns" << std::endl;
+  }
+}
+
+void report_solution(int const solution) {
+  std::cout << "Solution: " << solution << std::endl;
+}
