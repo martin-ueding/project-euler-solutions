0017d87ef864cfa9fdc34c4939ec924fa3c9de11
Martin Ueding <mu@martin-ueding.de>
2026-05-14 17:49:43 +0200

    feat(Rust): start with Rust version

---
+++ project-euler-rust/src/main.rs
@@ -0,0 +1,6 @@
+mod solution_1;
+
+fn main() {
+    let solution = solution_1::solution_1();
+    println!("Solution: {solution}");
+}
---
+++ project-euler-rust/src/solution_1.rs
@@ -0,0 +1,9 @@
+pub fn solution_1() -> i32 {
+    let mut sum_of_multiples = 0;
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum_of_multiples += i;
+        }
+    }
+    sum_of_multiples
+}
