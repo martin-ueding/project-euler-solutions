8e225da19dd6b7cdeed72350e3d6f55219f79bb9
Martin Ueding <mu@martin-ueding.de>
2026-05-14 18:04:44 +0200

    feat(Rust): add time measurement

--- project-euler-rust/src/main.rs
+++ project-euler-rust/src/main.rs
@@ -1,6 +1,36 @@
 mod solutions;
 
+use std::time::Instant;
+
+fn measure<F>(f: F) -> i32
+where
+    F: Fn() -> i32,
+{
+    let start = Instant::now();
+    let result = f();
+    let elapsed = start.elapsed();
+
+    let duration = format_duration(elapsed.as_secs_f64());
+    println!("Duration: {duration}");
+
+    result
+}
+
+fn format_duration(seconds: f64) -> String {
+    let ms = seconds * 1_000.0;
+    let us = seconds * 1_000_000.0;
+    let ns = seconds * 1_000_000_000.0;
+
+    if ms > 1.0 {
+        format!("{ms:.3} ms")
+    } else if us > 1.0 {
+        format!("{us:.3} µs")
+    } else {
+        format!("{ns:.3} ns")
+    }
+}
+
 fn main() {
-    let solution = solutions::s1::solution_1();
+    let solution = measure(solutions::s1::solution_1);
     println!("Solution: {solution}");
 }
