f37759e4fc9bd666d12455b747dd989357ff8d0e
Martin Ueding <mu@martin-ueding.de>
2026-05-14 21:08:05 +0200

    refactor(Rust): pull out prime generator

--- project-euler-rust/src/main.rs
+++ project-euler-rust/src/main.rs
@@ -1,3 +1,4 @@
+mod primes;
 mod registry;
 mod solutions;
 
---
+++ project-euler-rust/src/primes.rs
@@ -0,0 +1,52 @@
+pub struct PrimeGenerator {
+    primes: Vec<i64>,
+}
+
+impl PrimeGenerator {
+    pub fn new() -> Self {
+        PrimeGenerator { primes: vec![2] }
+    }
+
+    pub fn iter(&mut self) -> PrimeIterator<'_> {
+        PrimeIterator {
+            primes: &mut self.primes,
+            index: 0,
+        }
+    }
+}
+
+pub struct PrimeIterator<'a> {
+    primes: &'a mut Vec<i64>,
+    index: usize,
+}
+
+impl<'a> Iterator for PrimeIterator<'a> {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.primes.len() {
+            let result = self.primes[self.index];
+            self.index += 1;
+            return Some(result);
+        } else {
+            let mut candidate = self.primes.last()? + 1;
+
+            loop {
+                let mut is_prime = true;
+                for prime in self.primes.iter() {
+                    if candidate % *prime == 0 {
+                        is_prime = false;
+                        break;
+                    }
+                }
+                if is_prime {
+                    self.primes.push(candidate);
+                    // println!("{0:?}", self.primes);
+                    self.index += 1;
+                    return Some(candidate);
+                }
+                candidate += 1;
+            }
+        }
+    }
+}
