49d24b76ded799418f7701129e008492fa5698a9
Martin Ueding <mu@martin-ueding.de>
2026-05-25 17:23:54 +0200

    feat(Rust): implement gcd

--- rust/src/primes.rs
+++ rust/src/primes.rs
@@ -105,6 +105,15 @@ pub fn get_num_divisors(number: i64, prime_generator: &mut PrimeList) -> i64 {
         .fold(1, |acc: i64, (&_prime, &count)| acc * (count + 1))
 }
 
+fn greatest_common_denominator(mut a: i64, mut b: i64) -> i64 {
+    while b != 0 {
+        let c = a % b;
+        a = b;
+        b = c;
+    }
+    a
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -145,5 +154,10 @@ mod tests {
     fn test_get_num_divisors() {
         let mut prime_generator = PrimeList::new();
         assert_eq!(get_num_divisors(28, &mut prime_generator), 6);
+    }
+
+    #[test]
+    fn test_greatest_common_denominator() {
+        assert_eq!(greatest_common_denominator(1071, 462), 21);
     }
 }
