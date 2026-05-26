3013f97abd4ef413b76297905f0334b52c0a8f1b
Martin Ueding <mu@martin-ueding.de>
2026-05-26 09:46:43 +0200

    chore(Rust): don't make solutions public

--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -2,18 +2,18 @@
 //!
 //! Code that is reusable should go into the library files. Everything else that is specific to a given problem should go into the solution module.
 
-pub mod solution_1;
-pub mod solution_10;
+mod solution_1;
+mod solution_10;
 mod solution_100;
-pub mod solution_12;
-pub mod solution_14;
-pub mod solution_2;
-pub mod solution_3;
-pub mod solution_4;
-pub mod solution_5;
-pub mod solution_6;
-pub mod solution_64;
-pub mod solution_65;
-pub mod solution_7;
-pub mod solution_8;
-pub mod solution_90;
+mod solution_12;
+mod solution_14;
+mod solution_2;
+mod solution_3;
+mod solution_4;
+mod solution_5;
+mod solution_6;
+mod solution_64;
+mod solution_65;
+mod solution_7;
+mod solution_8;
+mod solution_90;
