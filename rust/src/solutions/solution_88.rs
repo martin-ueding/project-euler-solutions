323d4880bbb3f0c042f890d19e76959cbe476269
Martin Ueding <mu@martin-ueding.de>
2026-06-02 21:53:28 +0200

    chore: delete old attempts

--- rust/src/solutions/solution_88.rs
+++ rust/src/solutions/solution_88.rs
@@ -1,39 +1,7 @@
-use indicatif::ProgressIterator;
 use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use crate::primes::{PrimeList, get_factorizations};
-
-fn possible_products(n: i64, k: i64) -> HashSet<i64> {
-    if k == 1 {
-        HashSet::from([n])
-    } else {
-        let mut result: HashSet<i64> = HashSet::new();
-        for i in 1..n - k + 2 {
-            possible_products(n - i, k - 1).iter().for_each(|p| {
-                // println!("n={n}, k={k}, i={i}, p={p}");
-                result.insert(i * p);
-            });
-        }
-        result
-    }
-}
-
-fn minimal_product_for_k(k: i64) -> i64 {
-    for n in 2.. {
-        if possible_products(n, k).contains(&n) {
-            return n;
-        }
-    }
-    return -1;
-}
-
-fn solution_fixed_k() -> i64 {
-    (2..12_001)
-        .progress_count(11_999)
-        .map(minimal_product_for_k)
-        .sum()
-}
 
 fn solution_fixed_n() -> i64 {
     let mut prime_generator = PrimeList::new();
@@ -82,20 +50,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_possible_products() {
-        assert_eq!(possible_products(1, 1), HashSet::from([1]));
-        assert_eq!(possible_products(2, 1), HashSet::from([2]));
-        assert_eq!(possible_products(2, 2), HashSet::from([1]));
-        assert_eq!(possible_products(3, 2), HashSet::from([2]));
-        assert_eq!(possible_products(4, 2), HashSet::from([3, 4]));
-    }
-
-    #[test]
-    fn test_minimal_product_for_k() {
-        assert_eq!(minimal_product_for_k(2), 4);
-        assert_eq!(minimal_product_for_k(3), 6);
-        assert_eq!(minimal_product_for_k(4), 8);
-        assert_eq!(minimal_product_for_k(5), 8);
-        assert_eq!(minimal_product_for_k(6), 12);
+    fn test_solution_fixed_n() {
+        assert_eq!(solution_fixed_n(), 7_587_457);
     }
 }
