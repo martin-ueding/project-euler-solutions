e0e2ce4dc9cc4314aea1bf56e0339c667583115b
Martin Ueding <mu@martin-ueding.de>
2026-06-04 21:53:28 +0200

    perf: make 95 way faster

--- rust/src/solutions/solution_95.rs
+++ rust/src/solutions/solution_95.rs
@@ -66,10 +66,66 @@ fn solution() -> i64 {
         .1
 }
 
+fn divisor_sums() -> Vec<i64> {
+    let mut result: Vec<i64> = vec![0; 1_000_001];
+    for divisor in 1..1_000_001i64 {
+        for multiplier in 2.. {
+            let index = divisor * multiplier;
+            if index >= result.len() as i64 {
+                break;
+            }
+            result[index as usize] += divisor;
+        }
+    }
+    result
+}
+
+fn get_chain_length(start: i64, next: &mut Vec<i64>) -> Option<(i64, i64)> {
+    // println!("start = {start}, next[12496] = {}", next[12496]);
+    let mut explored: HashSet<i64> = HashSet::new();
+    let mut number = start;
+    loop {
+        if start == 12496 {
+            // println!("explored = {:?}, number = {number}", explored);
+        }
+        explored.insert(number);
+        number = next[number as usize];
+        if start == 12496 {
+            // println!("  explored = {:?}, number = {number}", explored);
+        }
+        if number == 0 || number > 1_000_000 || explored.contains(&number) {
+            break;
+        }
+    }
+    if start == 12496 {
+        // println!("explored = {:?}", explored);
+    }
+    if number == start {
+        let chain_length = explored.len() as i64;
+        let min_element = *explored.iter().min().unwrap();
+        // println!("start = {start}, length = {chain_length}, min = {min_element}, {explored:?}");
+        return Some((chain_length, min_element));
+    } else if number == 0 {
+        explored.into_iter().for_each(|n| next[n as usize] = 0);
+    }
+    None
+}
+
+fn solution_sieve() -> i64 {
+    let mut next = divisor_sums();
+    // println!("{:?}", &next[0..100]);
+    (1..1_000_001)
+        .map(|start| get_chain_length(start, &mut next))
+        .flatten()
+        .max()
+        .unwrap()
+        .1
+}
+
 inventory::submit! {
     crate::registry::SolutionEntry {
         id: 95,
-        implementations: &[("", solution)],
+        implementations: &[("", solution_sieve)],
     }
 }
 
