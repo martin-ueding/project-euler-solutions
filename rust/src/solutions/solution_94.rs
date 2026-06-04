cfabfbb7c0700a32482c22a3237de9f960771a75
Martin Ueding <mu@martin-ueding.de>
2026-06-04 14:29:00 +0200

    fix: make it more robust

--- rust/src/solutions/solution_94.rs
+++ rust/src/solutions/solution_94.rs
@@ -4,30 +4,27 @@ fn solution() -> i64 {
     let mut prime_list = PrimeList::new();
     let mut result = 0;
     for (x, y) in DiophantineSolutionIterator::new(3, 4, &mut prime_list) {
-        println!("x = {x}, y = {y}");
-
         let a_factor = 3;
-
-        if (x + 1) / a_factor > 1_000_000_000 {
-            break;
-        }
 
         // The case (a, a, a+1).
         if (x + 1) % a_factor == 0 {
             let a = (x + 1) / a_factor;
+            if (y * (a + 1)) % 4 != 0 {
+                continue;
+            }
             let p = 3 * a + 1;
-            println!("  a = {a}, p = {p}");
             if p > 1_000_000_000 {
                 break;
             }
             result += p;
         }
-
         // The case (a, a, a-1).
-        if (x - 1) % a_factor == 0 {
+        else if (x - 1) % a_factor == 0 {
             let a = (x - 1) / a_factor;
+            if (y * (a - 1)) % 4 != 0 {
+                continue;
+            }
             let p = 3 * a - 1;
-            println!("  a = {a}, p = {p}");
             if p > 1_000_000_000 {
                 break;
             }
