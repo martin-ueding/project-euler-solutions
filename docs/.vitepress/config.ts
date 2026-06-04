de72b7d74ec9305002bc80e8846a04e3673d27ad
Martin Ueding <mu@martin-ueding.de>
2026-06-04 11:21:16 +0200

    docs: switch to VitePress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,26 @@
+import { withSidebar } from 'vitepress-sidebar'
+
+export default withSidebar(
+  {
+    title: "Martin's Project Euler Solutions",
+    description: 'My Project Euler solutions.',
+    base: '/project-euler-solutions/',
+    markdown: {
+      math: true,
+    },
+    themeConfig: {
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/martin-ueding/project-euler-solutions' },
+      ],
+      footer: {
+        copyright: 'Copyright &copy; 2023–2026 Martin Ueding',
+      },
+    },
+  },
+  {
+    documentRootPath: 'docs',
+    useTitleFromFileHeading: true,
+    sortMenusByName: true,
+    collapsed: false,
+  },
+)
