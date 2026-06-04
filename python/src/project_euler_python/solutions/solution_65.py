cfe8fb5f74f31072f51699f5ee7c8cfef6f67167
Martin Ueding <mu@martin-ueding.de>
2026-06-04 18:52:02 +0200

    fix: fix some older python code

--- python/src/project_euler_python/solutions/solution_65.py
+++ python/src/project_euler_python/solutions/solution_65.py
@@ -22,18 +22,18 @@ def continued_fraction_e() -> Iterator[int]:
 
 
 def convergent_from_sequence(coefficients: list[int]) -> tuple[int, int]:
-    denominoator = 1
+    denominator = 1
     numerator = coefficients[-1]
     for coefficient in reversed(coefficients[:-1]):
-        numerator, denominoator = denominoator, numerator
-        numerator += coefficient * denominoator
-    gcd = greatest_common_denominator(numerator, denominoator)
+        numerator, denominator = denominator, numerator
+        numerator += coefficient * denominator
+    gcd = greatest_common_denominator(numerator, denominator)
     numerator //= gcd
-    denominoator //= gcd
-    return numerator, denominoator
+    denominator //= gcd
+    return numerator, denominator
 
 
-def convergents_series(coefficients: Iterator[int]) -> Iterator[int]:
+def convergents_series(coefficients: Iterator[int]) -> Iterator[tuple[int, int]]:
     coefficients_so_far = []
     for coefficient in coefficients:
         coefficients_so_far.append(coefficient)
