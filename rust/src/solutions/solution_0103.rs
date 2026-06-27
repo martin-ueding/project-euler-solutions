b2efad068f052ee6740237d792e61de76fb923df
Martin Ueding <mu@martin-ueding.de>
2026-06-27 09:50:34 +0200

    chore: add some bottom-up functions

--- rust/src/solutions/solution_0103.rs
+++ rust/src/solutions/solution_0103.rs
@@ -1,5 +1,37 @@
+use itertools::Itertools;
+
 fn optimal_special_set(n: i64) -> Vec<i64> {
     vec![]
+}
+
+fn is_unequal_subsets(b: &[i64], c: &[i64]) -> bool {
+    b.iter().sum::<i64>() != c.iter().sum::<i64>()
+}
+
+fn satisfies_larger_constraint(a: &[i64]) -> bool {
+    (1..(a.len() + 1) / 2)
+        .all(|k| a[..k + 1].iter().sum::<i64>() >= a[a.len() - k..].iter().sum::<i64>())
+}
+
+fn is_valid_permutation(a: &[i64]) -> bool {
+    for m in 1..a.len() - 1 {
+        for n in 1..a.len() - m {
+            let b = &a[..m];
+            let c = &a[m..m + n];
+            if !is_unequal_subsets(&b, &c) {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn is_special_sum_set(a: &[i64]) -> bool {
+    satisfies_larger_constraint(a)
+        && a.iter()
+            .copied()
+            .permutations(a.len())
+            .all(|p| is_valid_permutation(&p))
 }
 
 #[cfg(test)]
@@ -34,5 +66,25 @@ mod tests {
     #[test]
     fn test_optimal_special_set_6() {
         assert_eq!(optimal_special_set(6), vec![11, 18, 19, 20, 22, 25]);
+    }
+
+    #[test]
+    fn test_is_special_sum_set_example_6() {
+        assert!(is_special_sum_set(&vec![6, 9, 11, 12, 13]));
+    }
+
+    #[test]
+    fn test_is_satisfies_larger_constraint_1() {
+        assert!(satisfies_larger_constraint(&vec![3, 5, 6, 7]));
+    }
+
+    #[test]
+    fn test_is_satisfies_larger_constraint_2() {
+        assert!(satisfies_larger_constraint(&vec![6, 9, 11, 12, 13]));
+    }
+
+    #[test]
+    fn test_is_satisfies_larger_constraint_3() {
+        assert!(!satisfies_larger_constraint(&vec![1, 2, 3, 5]));
     }
 }
