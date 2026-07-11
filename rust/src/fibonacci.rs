56f26d38325b2f27a98e169f290d97de5fd5c374
Martin Ueding <mu@martin-ueding.de>
2026-07-11 17:03:18 +0200

    feat: separate overflowing Fibonacci generator

--- rust/src/fibonacci.rs
+++ rust/src/fibonacci.rs
@@ -14,7 +14,30 @@ impl Iterator for FibonacciIterator {
 
     fn next(&mut self) -> Option<Self::Item> {
         let result = self.b;
-        let c = self.a.overflowing_add(self.b).0;
+        let c = self.a + self.b;
+        self.a = self.b;
+        self.b = c;
+        Some(result)
+    }
+}
+
+pub struct OverflowingFibonacciIterator {
+    a: u64,
+    b: u64,
+}
+
+impl OverflowingFibonacciIterator {
+    pub fn new() -> Self {
+        OverflowingFibonacciIterator { a: 0, b: 1 }
+    }
+}
+
+impl Iterator for OverflowingFibonacciIterator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = self.b;
+        let c = self.a.wrapping_add(self.b);
         self.a = self.b;
         self.b = c;
         Some(result)
@@ -29,6 +52,16 @@ mod tests {
     fn fibonacci_iterator_first_10() {
         assert_eq!(
             FibonacciIterator::new().take(10).collect::<Vec<i64>>(),
+            vec![1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+        );
+    }
+
+    #[test]
+    fn overflowing_fibonacci_iterator_first_10() {
+        assert_eq!(
+            OverflowingFibonacciIterator::new()
+                .take(10)
+                .collect::<Vec<u64>>(),
             vec![1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
         );
     }
