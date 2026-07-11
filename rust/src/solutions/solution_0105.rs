1385580ca0535ebff1060b745b61607718354890
Martin Ueding <mu@martin-ueding.de>
2026-07-11 20:36:08 +0200

    implement naive solution for 105

--- rust/src/solutions/solution_0105.rs
+++ rust/src/solutions/solution_0105.rs
@@ -1,11 +1,19 @@
 use std::fs;
 
+use indicatif::ProgressIterator;
+
+use crate::special_sum_sets::is_special_sum_set;
+
 fn solution() -> i64 {
     let sets = load_data();
-    0
+    sets.iter()
+        .progress()
+        .filter(|s| is_special_sum_set(*s))
+        .map(|s| s.iter().sum::<i32>())
+        .sum::<i32>() as i64
 }
 
-fn load_data() -> Vec<Vec<i64>> {
+fn load_data() -> Vec<Vec<i32>> {
     fs::read_to_string("../data/0105_sets.txt")
         .expect("File couldn't be found!")
         .lines()
@@ -13,15 +21,15 @@ fn load_data() -> Vec<Vec<i64>> {
         .collect()
 }
 
-fn line_to_vec(line: &str) -> Vec<i64> {
+fn line_to_vec(line: &str) -> Vec<i32> {
     line.split(',')
-        .map(|s| s.trim().parse::<i64>().unwrap())
+        .map(|s| s.trim().parse::<i32>().unwrap())
         .collect()
 }
 
 inventory::submit! {
     crate::registry::SolutionEntry {
-        id: 104,
+        id: 105,
         implementations: &[
             ("", solution)
         ]
