4cc82cecb9f87ebe16ff35f5fe66f9d996f77533
Martin Ueding <mu@martin-ueding.de>
2026-07-11 17:59:44 +0200

    feat: add big Fibonacci iterator

--- rust/src/fibonacci.rs
+++ rust/src/fibonacci.rs
@@ -1,3 +1,6 @@
+use num_bigint::BigInt;
+use num_traits::{One, Zero};
+
 pub struct FibonacciIterator {
     a: i64,
     b: i64,
@@ -44,6 +47,30 @@ impl Iterator for FibonacciSuffixIterator {
     }
 }
 
+pub struct BigFibonacciIterator {
+    a: BigInt,
+    b: BigInt,
+}
+
+impl BigFibonacciIterator {
+    pub fn new() -> Self {
+        BigFibonacciIterator {
+            a: BigInt::zero(),
+            b: BigInt::one(),
+        }
+    }
+}
+
+impl Iterator for BigFibonacciIterator {
+    type Item = BigInt;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let c = &self.a + &self.b;
+        self.a = std::mem::replace(&mut self.b, c);
+        Some(self.a.clone())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,12 +84,25 @@ mod tests {
     }
 
     #[test]
-    fn overflowing_fibonacci_iterator_first_10() {
+    fn fibonacci_suffix_iterator_first_10() {
         assert_eq!(
             FibonacciSuffixIterator::new()
                 .take(10)
                 .collect::<Vec<i64>>(),
             vec![1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+        );
+    }
+
+    #[test]
+    fn big_fibonacci_iterator_first_10() {
+        assert_eq!(
+            BigFibonacciIterator::new()
+                .take(10)
+                .collect::<Vec<BigInt>>(),
+            vec![1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+                .into_iter()
+                .map(BigInt::from)
+                .collect::<Vec<BigInt>>()
         );
     }
 }
